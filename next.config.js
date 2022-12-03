ecf3f86c2fe2c81197beab7ae54344528f3bbd3c
AdiCore <56226709+adicore@users.noreply.github.com>
2022-12-03 07:36:35 +0700

    update

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-    experimental: {
-        runtime: "experimental-edge",
-        appDir: true,
-      },
+    // experimental: {
+    //     runtime: "experimental-edge",
+    //     appDir: true,
+    //   },
     reactStrictMode: true,
     swcMinify: true,
 
