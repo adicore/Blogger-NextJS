166a0238738a7942d366186ec1363e6d66ad957d
AdiCore <56226709+adicore@users.noreply.github.com>
2022-12-03 07:25:05 +0700

    update

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -15,12 +15,17 @@
 // Next.js Edge API Routes: https://nextjs.org/docs/api-routes/edge-api-routes
 import type { NextRequest } from "next/server";
 
+type Data = {
+  name: string
+}
+
 export const config = {
   runtime: "experimental-edge",
 };
 
 export default async function handler(
-  req: NextRequest
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
 ): Promise<Response> {
   return new Response(JSON.stringify({ name: "John Doe" }), {
     status: 200,
